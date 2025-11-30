60fb1de9f70d39416aac2b5f7de3ed92635db2f3
sathiya <nsathiya757@gmail.com>
2025-11-30 14:54:26 +0530

    Goal router created and create CRUD api's

---
+++ src/routes/goal-router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { isAuthenticated } from '../middleware/authorization-middleware.js';
+import { createGoal, deleteGoal, filterGoal, findAllGoal, findOne, updateGoal } from '../controller/goal-controller.js';
+
+export const goalRouter = express.Router();
+
+goalRouter.post("/add",isAuthenticated,createGoal);
+
+goalRouter.get("/find",isAuthenticated,findAllGoal);
+
+goalRouter.get("/get/:id",isAuthenticated,findOne);
+
+goalRouter.put("/update/:id",isAuthenticated,updateGoal);
+
+goalRouter.delete("/delete/:id",isAuthenticated,deleteGoal);
+
+goalRouter.post('/filter',isAuthenticated,filterGoal);
+
