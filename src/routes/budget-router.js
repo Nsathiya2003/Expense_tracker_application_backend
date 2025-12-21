44788e302dd65e219356f6c71e85e0d47287884a
sathiya <nsathiya757@gmail.com>
2025-12-21 22:15:02 +0530

    Budget model and router is created

--- src/routes/budget-router.js
+++ src/routes/budget-router.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/authorization-middleware.js";
+import { createBudget, deleteBudget, filterBudget, getBudgetById, getBudgets, updateBudget } from "../controller/budget-controller.js";
 
 const budgetRouter = express.Router();
 
-budgetRouter.post('/create',isAuthenticated);
+budgetRouter.post('/create',isAuthenticated,createBudget);
 
-budgetRouter.get('/find',isAuthenticated);
+budgetRouter.get('/find',isAuthenticated,getBudgets);
 
-budgetRouter.get('/find/:id',isAuthenticated);
+budgetRouter.get('/get/:id',isAuthenticated,getBudgetById);
 
-budgetRouter.put('/update/:id',isAuthenticated);
+budgetRouter.put('/update/:id',isAuthenticated,updateBudget);
 
-budgetRouter.delete('/delete/:id',isAuthenticated);
+budgetRouter.delete('/delete/:id',isAuthenticated,deleteBudget);
 
-budgetRouter.post('/filter',isAuthenticated);
+budgetRouter.post('/filter',isAuthenticated,filterBudget);
 
 export default budgetRouter;
